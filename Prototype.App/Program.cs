93bc6d8206899feeb75ef13c5fa6adc6456d6a92
Starinin Andrey <starininandrey@gmail.com>
2022-02-14 10:58:22 +0300

    Init proj

---
+++ Prototype.App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Prototype.Lib;
+
+namespace Prototype.App
+{
+    class Program
+    {
+        static void Main()
+        {
+            var user1 = new User("Andrey", 35);
+            var user2 = new User(user1);
+            Console.WriteLine($"{user1.GetName()}, {user1.GetAge()}");
+            Console.WriteLine($"{user2.GetName()}, {user2.GetAge()}");
+
+            var user3 = user2.Clone() as User;
+            Console.WriteLine($"{user3?.GetName()}, {user3?.GetAge()}");
+            
+            var user4 = Cloning(user3) as User;
+            Console.WriteLine($"{user4?.GetName()}, {user4?.GetAge()}");
+        }
+
+        static object Cloning(ICloneable cloneable)
+        {
+            //add logging
+            return cloneable.Clone();
+        }
+    }
+}
---
+++ Prototype.Lib/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Prototype.Lib
+{
+    public class User : ICloneable
+    {
+        private readonly string _name;
+        private readonly int _age;
+
+        public User(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public User(User user)
+        {
+            _name = user.GetName();
+            _age = user.GetAge();
+        }
+
+        public string GetName() => _name;
+        public int GetAge() => _age;
+        
+        public object Clone()
+        {
+            return new User(this);
+        }
+    }
+}
