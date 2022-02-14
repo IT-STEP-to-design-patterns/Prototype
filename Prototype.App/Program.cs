722e3aeb24ef505b146360a8f4bd6f9704bc5097
Starinin Andrey <starininandrey@gmail.com>
2022-02-14 11:01:29 +0300

    Update

--- Prototype.App/Program.cs
+++ Prototype.App/Program.cs
@@ -3,20 +3,20 @@ using Prototype.Lib;
 
 namespace Prototype.App
 {
-    class Program
+    public static class Program
     {
-        static void Main()
+        private static void Main()
         {
             var user1 = new User("Andrey", 35);
             var user2 = new User(user1);
-            Console.WriteLine($"{user1.GetName()}, {user1.GetAge()}");
-            Console.WriteLine($"{user2.GetName()}, {user2.GetAge()}");
+            Console.WriteLine($"{user1.GetName()}, {user1.GetAge().ToString()}");
+            Console.WriteLine($"{user2.GetName()}, {user2.GetAge().ToString()}");
 
             var user3 = user2.Clone() as User;
-            Console.WriteLine($"{user3?.GetName()}, {user3?.GetAge()}");
+            Console.WriteLine($"{user3?.GetName()}, {user3?.GetAge().ToString()}");
             
             var user4 = Cloning(user3) as User;
-            Console.WriteLine($"{user4?.GetName()}, {user4?.GetAge()}");
+            Console.WriteLine($"{user4?.GetName()}, {user4?.GetAge().ToString()}");
         }
 
         static object Cloning(ICloneable cloneable)
